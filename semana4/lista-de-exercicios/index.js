820a9a506e51d760755bc59b946ac90c4a4a841e
Natália Acedo <n.alves.acedo@gmail.com>
2020-03-13 11:50:51 -0300

    interpretação - ex3

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -7,3 +7,8 @@
 //2
 //console.log(novoMontante) imprimirá 165
 //console.log(segundoMontante) imprimirá o alert("TIPO DE INVESTIMENTO INFORMADO INCORRETO")
+
+//3
+//console.log("Quantidade total de números", numeros.length) imprimirá "Quantidade total de números" e o comprimento do array numeros(14).
+//console.log(array1.length) imprimirá o comprimento do array1, que o array com os números pares do array "numeros"
+//console.log(array2.length) imprimirá o comprimento do array2, que o array com os números ímpares do array "numeros"
