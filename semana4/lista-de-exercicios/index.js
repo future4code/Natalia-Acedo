6c2cd154e9dfe5425f0e77ce663e49e663e70351
Natália Acedo <n.alves.acedo@gmail.com>
2020-03-13 11:44:07 -0300

    interpretação - ex2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -3,3 +3,7 @@
 
 //1 
 //O console retornará quanto é o valor em reais de 100 dólares
+
+//2
+//console.log(novoMontante) imprimirá 165
+//console.log(segundoMontante) imprimirá o alert("TIPO DE INVESTIMENTO INFORMADO INCORRETO")
