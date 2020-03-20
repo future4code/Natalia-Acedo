ff8e6b560b9a40d657e61bae734202d904a43d0c
Natália Acedo <n.alves.acedo@gmail.com>
2020-03-19 21:37:40 -0300

    Exercício 2

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -28,11 +28,79 @@ class App extends React.Component {
       valorInputNome: "",
       valorInputFotoUsuario: "",
       valorInputFotoPost: ""
-
     }
   }
 
-    render() {
+  adicionaPost = () => {
+    const novoPost ={
+      nomeUsuario: this.state.valorInputNome,
+      fotoUsuario: this.state.valorInputFotoUsuario,
+      fotoPost: this.state.valorInputFotoPost
+    }
+
+    const novoPostCopia = [...this.state.posts, novoPost]
+
+    this.setState({
+      posts: novoPostCopia,
+      valorInputNome: "",
+      valorInputFotoUsuario: "",
+      valorInputFotoPost: ""
+    })
+  }
+
+  onChangeInputNome = event => {
+    this.setState({ valorInputNome: event.target.value });
+  };
+
+  onChangeInputFotoUsuario = event => {
+    this.setState({ valorInputFotoUsuario: event.target.value });
+  };
+
+  onChangeInputFotoPost = event => {
+    this.setState({ valorInputFotoPost: event.target.value });
+  };
+
+
+  render() {
+    const feedPost = this.state.posts.map((post, index) => {
+      return (
+        <div className={'app-container'}>
+          <Post
+            nomeUsuario={post.nomeUsuario}
+            fotoUsuario={post.fotoUsuario}
+            fotoPost={post.fotoPost}
+            key={index}
+          />
+        </div>
+        )
+      })
+      return (
+        <div className={'app-container'}>
+          <input 
+          value={this.state.valorInputNome} 
+          onChange={this.onChangeInputNome}
+          placeholder="Nome"
+          />
+
+          <input 
+          value={this.state.valorInputFotoUsuario}
+          onChange={this.onChangeInputFotoUsuario}
+          placeholder="Foto do usuário"
+          />
+
+          <input 
+          value={this.state.valorInputFotoPost} 
+          onChange={this.onChangeInputFotoPost}
+          placeholder="Foto do post"
+          />
+          <button onClick={this.adicionaPost}>Enviar post</button>
+          <div> {feedPost} </div>
+        </div>
+      )
+  }
+}
+
+    /* render() {
     return (
       this.state.posts.map((post, index) => {
         return (
@@ -48,6 +116,6 @@ class App extends React.Component {
       })
     )
   }
-}
+} */
 
 export default App;
